85b4e148e20acd05c4294ec1a6e9754d1ea79f83
Prasanth AV <71940769+Prasanth123456-boo@users.noreply.github.com>
2022-12-22 14:18:55 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,25 @@
-package com.example.inshare;
+package com.pandanadroversapplications.student;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import android.content.Intent;
-import  android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText e1,e2;
-    AppCompatButton b1,b2;
-    String getUserName,getPassword,PrefValue;
-    SharedPreferences mypreferances;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mypreferances=getSharedPreferences("login",MODE_PRIVATE);
-        e1=findViewById(R.id.name);
-        e2=findViewById(R.id.pswd);
-        b1=findViewById(R.id.signin);
-        b2=findViewById(R.id.signout);
-        PrefValue=mypreferances.getString("email",null);
-        if(PrefValue!=null){
 
-            Intent i=new Intent(getApplicationContext(),DashboardActivity.class);
-            startActivity(i);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        }
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getUserName=e1.getText().toString();
-                getPassword=e2.getText().toString();
-                if(getUserName.equals("abc12@gmail.com")&&getPassword.equals("12345")){
-                    SharedPreferences.Editor myedit=mypreferances.edit();
-                    myedit.putString("email",getUserName);
-                    myedit.commit();
-
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"invalid credential",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences.Editor myedit=mypreferances.edit();
-                myedit.clear();
-                myedit.commit();
-            }
-        });
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this,Entry.class));
+            finish();
+        }, 2000);
     }
 }
